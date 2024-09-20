bf28ca3478b2b7fd4b17b447b75abeac51d354e0
Neel <neel.vikmani@zeptonow.com>
2024-09-20 15:52:16 +0530

    browse headless

--- selenium_helper.py
+++ selenium_helper.py
@@ -13,13 +13,13 @@ from selenium.webdriver.support import expected_conditions as EC
 def setup_browser():
     options = webdriver.ChromeOptions()
     #
-    # options.add_argument("--headless")
-    # options.add_argument('--no-sandbox')
-    # options.add_argument("--disable-gpu")
-    # options.add_argument("--window-size=1920,1080")
-    # options.add_argument("--disable-dev-shm-usage")
-    # options.add_argument("--disable-dev-tools")
-    # options.add_argument("--no-zygote")
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
 
     driver = webdriver.Chrome(options=options)
     return driver
